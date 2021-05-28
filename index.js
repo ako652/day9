6483dfe9690f9872aa1670a6d559c237eba5f177
ako652 <akotabe0213@gmail.com>
2021-05-28 13:29:33 +0200

    random colors
    selecting list of colors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ AddingNewCells()
 
 
 
-const AddingNewCells= function(){
+    const AddingNewCells= function(){
 
     let IdContainer=document.getElementById("container")
     IdContainer.innerHTML=""
@@ -17,20 +17,8 @@ const AddingNewCells= function(){
         let newdiv=document.createElement("div")
         newdiv.innerText=i
         newdiv.classList.add("cells")
-        newdiv.onclick = makeItClickable()
+    
     IdContainer.appendChild(newdiv)
         
+       }
     }
-
-}
-
-
-function makeItClickable() {
-    let myColors = ['red', 'purple', '#E84751', 'blue', 'orange', '#323643'];
-    document.querySelector("button").addEventListener('click', function(){
-      for(i = 0; i < myColors.length; i++) {
-        this .style.color = myColors[i];
-      }
-    });
-  }
-    
